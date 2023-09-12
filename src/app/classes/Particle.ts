34eeb55f2dc579b17b03958dcee115cbc4d72d5e
insecker <pierresecher98@gmail.com>
2023-09-12 15:48:08 +0200

    UI + multiple programs

--- src/app/classes/Particle.ts
+++ src/app/classes/Particle.ts
@@ -8,54 +8,119 @@ export class Particle {
     velY: number;
     radius: number;
     color: string;
+    bound: number;
 
-    constructor(x: number, y: number, radius: number) {
+    constructor(x: number, y: number, radius: number, sphereRadius: number) {
         this.x = x;
         this.y = y;
-        this.velX = 0;
-        this.velY = 0;
+        this.velX = Math.random() - 0.5;
+        this.velY = Math.random() - 0.5;
         this.radius = getRandomNumber(1, 2);
         this.color = "rgba(255,255,255,0.6)";
+        this.bound = sphereRadius / 2 - 100;
     }
 
-    render(
+    update(newBound: number) {
+        this.bound = newBound / 2 - 100;
+    }
+
+    gravity(
+        ctx: CanvasRenderingContext2D,
+        centerX: number,
+        centerY: number,
+        randomness: number
+    ) {
+        printCircle(
+            this.x,
+            this.y,
+            this.color,
+            0.001 * this.y * (Math.abs(this.velX) + Math.abs(this.velY)),
+            ctx
+        );
+
+        this.velY *= 0.996;
+        this.velX *= 0.996;
+
+        this.velY += (Math.random() - 0.5) * randomness;
+        this.velX += (Math.random() - 0.5) * randomness;
+
+        let dist = Math.sqrt(
+            Math.pow(this.x - centerX, 2) + Math.pow(this.y - centerY, 2)
+        );
+
+        const maxDist = 150;
+        const repluseTrigger = dist - 200;
+
+        if (repluseTrigger > 0) {
+            this.velX +=
+                -((this.x - centerX) / dist) * (repluseTrigger / maxDist) * 0.2;
+            this.velY +=
+                -((this.y - centerY) / dist) * (repluseTrigger / maxDist) * 0.2;
+        }
+
+        if (this.y > centerY) {
+            this.velX -= 0.0001 * dist * (Math.abs(this.x) / centerY);
+        } else {
+            this.velX += 0.0001 * dist * (Math.abs(this.y) / centerX);
+        }
+
+        if (dist >= this.bound) {
+            this.velX = -this.velX * 0.2;
+            this.velY = -this.velY * 0.2;
+        }
+
+        // this.velX *= 0.99;
+
+        if (dist > this.bound) {
+            this.x = getRandomNumber(centerX - 50, centerX + 50);
+            this.y = getRandomNumber(centerY - 50, centerY + 50);
+            this.velX = 0;
+            this.velY = 0;
+        }
+
+        this.x += this.velX;
+        this.y += this.velY;
+    }
+
+    follow(
         ctx: CanvasRenderingContext2D,
         centerX: number,
         centerY: number,
         mouseVelX: number,
         mouseVelY: number
     ) {
-        // let red = ((this.y - (centerY - 250))/500)*255
-        // let green = (1-((this.y - (centerY - 250))/500))*83
-        // this.color = "rgb(" + red + "," + green + ",255)";
         printCircle(this.x, this.y, this.color, this.radius, ctx);
 
         this.x += this.velX;
         this.y += this.velY;
 
         this.velX += Math.random() - 0.5;
-        this.velX *= 0.8;
-        this.velY += Math.random() - 0.5 / 3 + 0.01;
+        this.velX *= 0.99;
+        this.velY += Math.random() - 0.5;
 
-        // this.velX += (mouseVelX / 5) * Math.random();
-        // this.velY += (mouseVelY / 5) * Math.random();
+        this.velX += (mouseVelX / 5) * Math.random();
+        this.velY += (mouseVelY / 5) * Math.random();
 
         let dist = Math.sqrt(
             Math.pow(this.x - centerX, 2) + Math.pow(this.y - centerY, 2)
         );
-        this.velX = (this.velX * dist) / 200;
-        this.velY = (this.velY * dist) / 200;
-        // if (
-        //     dist + Math.sqrt(Math.pow(this.velX, 2) + Math.pow(this.velY, 2)) >=
-        //     250
-        // ) {
-        //     this.velX = -this.velX / 3;
-        //     this.velY = -this.velY / 3;
-        // }
+        if (
+            dist + Math.sqrt(Math.pow(this.velX, 2) + Math.pow(this.velY, 2)) >=
+            this.bound
+        ) {
+            this.velX = -this.velX / 3;
+            this.velY = -this.velY / 3;
+        }
 
-        if (dist >= 251) {
-            this.x = getRandomNumber(centerX - 200, centerX + 200);
-            this.y = getRandomNumber(centerY - 200, centerY + 200);
+        if (dist >= this.bound + 1) {
+            this.x = getRandomNumber(
+                centerX - this.bound * 0.8,
+                centerX + this.bound * 0.8
+            );
+            this.y = getRandomNumber(
+                centerY - this.bound * 0.8,
+                centerY + this.bound * 0.8
+            );
         }
     }
 }
--- src/app/main.ts
+++ src/app/main.ts
@@ -2,79 +2,135 @@ import "destyle.css";
 import "../main.css";
 import { Particle } from "./classes/Particle";
 import { getRandomNumber } from "./helpers/functions";
+let width = window.innerWidth;
+let height = window.innerHeight;
+let centerX = width / 2;
+let centerY = height / 2;
+let initialized = false;
+
+// UI
+const followCursor = document.getElementById("follow-cursor") as HTMLDivElement;
+const randomnessDiv = document.getElementById("randomness") as HTMLDivElement;
+
+let currentProgram = 1;
+const programFollow = document.getElementById("follow") as HTMLInputElement;
+const programGravity = document.getElementById("gravity") as HTMLInputElement;
+
+programFollow.addEventListener("click", () => {
+    currentProgram = 1;
+    followCursor.classList.remove("hidden");
+    randomnessDiv.classList.add("hidden");
+});
+
+programGravity.addEventListener("click", () => {
+    currentProgram = 2;
+    followCursor.classList.add("hidden");
+    randomnessDiv.classList.remove("hidden");
+});
+
+const randomnessInput = document.getElementById("volume") as HTMLInputElement;
+let randomness = parseFloat(randomnessInput.value);
+
+randomnessInput.addEventListener("input", () => {
+    randomness = parseFloat(randomnessInput.value);
+});
+// UI END
+
+let mouseVelX = 0;
+let mouseVelY = 0;
+window.addEventListener("mousemove", (event) => {
+    let dist = Math.sqrt(
+        Math.pow(event.clientX - centerX, 2) +
+            Math.pow(event.clientY - centerY, 2)
+    );
+    mouseVelX = ((event.clientX - centerX) / dist) * 2;
+    mouseVelY = ((event.clientY - centerY) / dist) * 2;
+});
+
+let particles: Particle[] = [];
+for (let i = 0; i < 2000; i++) {
+    particles.push(
+        new Particle(
+            getRandomNumber(centerX - 150, centerX + 150),
+            getRandomNumber(centerY - 150, centerY + 150),
+            getRandomNumber(2, 3),
+            Math.min(width, height)
+        )
+    );
+}
 
 let canvas = document.getElementById("canvas") as HTMLCanvasElement;
-if (canvas !== null) {
-    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    canvas.width = width;
-    canvas.height = height;
-    const centerX = width / 2;
-    const centerY = height / 2;
+window.addEventListener("resize", () => {
+    width = window.innerWidth;
+    height = window.innerHeight;
+    centerX = width / 2;
+    centerY = height / 2;
 
-    // const purple = { red: 255, green: 0, blue: 255 };
-    // const blue = { red: 0, green: 83, blue: 255 };
-
-    let mouseVelX = 0;
-    let mouseVelY = 0;
-    window.addEventListener("mousemove", (event) => {
-        let dist = Math.sqrt(
-            Math.pow(event.clientX - centerX, 2) +
-                Math.pow(event.clientY - centerY, 2)
-        );
-        mouseVelX = (event.clientX - centerX) / dist;
-        mouseVelY = (event.clientY - centerY) / dist;
-    });
-
-    // function handleMotionEvent(event) {
-    //     mouseVelX = -event.accelerationIncludingGravity.x;
-    //     mouseVelY = event.accelerationIncludingGravity.y;
-    // }
-    // window.addEventListener("devicemotion", handleMotionEvent, true);
-
-    let particles: Particle[] = [];
-    for (let i = 0; i < 2000; i++) {
-        particles.push(
-            new Particle(
-                getRandomNumber(centerX - 50, centerX + 50),
-                getRandomNumber(centerY - 50, centerY + 50),
-                getRandomNumber(2, 3)
-            )
-        );
+    if (canvas !== null) {
+        canvas.width = width;
+        canvas.height = height;
     }
 
-    let pause = false;
-    window.addEventListener("keydown", (event) => {
-        if (event.code === "KeyP") {
-            pause = !pause;
-            if (!pause) {
-                loop();
-            }
-        }
+    if (width > 500 && !initialized) {
+        init();
+    }
+
+    const bound = Math.min(width, height);
+
+    particles.forEach((particle) => {
+        particle.update(bound);
     });
+});
+function init() {
+    if (canvas !== null) {
+        initialized = true;
+        const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+        canvas.width = width;
+        canvas.height = height;
 
-    const loop = function () {
-        ctx.fillStyle = "rgba(0,0,0,0.5)";
-        ctx.fillRect(0, 0, width, height);
-        mouseVelX *= 0.99;
-        mouseVelY *= 0.99;
-
-        particles.forEach((particle) => {
-            particle.render(ctx, centerX, centerY, mouseVelX, mouseVelY);
+        let pause = false;
+        window.addEventListener("keydown", (event) => {
+            if (event.code === "KeyP") {
+                pause = !pause;
+                if (!pause) {
+                    loop();
+                }
+            }
         });
 
-        ctx.fillStyle = "rgba(0,0,0,1)";
-        ctx.beginPath();
-        ctx.arc(centerX, centerY, 252 + 100, 0, 2 * Math.PI, false);
-        ctx.lineWidth = 200;
-        ctx.stroke();
+        const loop = function () {
+            ctx.fillStyle = "rgba(0,0,0,1)";
+            ctx.fillRect(0, 0, width, height);
 
-        if (!pause) {
-            requestAnimationFrame(loop);
-        }
-    };
+            mouseVelX *= 0.95;
+            mouseVelY *= 0.95;
 
-    loop();
+            if (currentProgram === 1) {
+                particles.forEach((particle) => {
+                    particle.follow(
+                        ctx,
+                        centerX,
+                        centerY,
+                        mouseVelX,
+                        mouseVelY
+                    );
+                });
+            } else if (currentProgram === 2) {
+                particles.forEach((particle) => {
+                    particle.gravity(ctx, centerX, centerY, randomness);
+                });
+            }
+
+            if (!pause) {
+                requestAnimationFrame(loop);
+            }
+        };
+
+        loop();
+    }
+}
+
+if (width > 500 && !initialized) {
+    init();
 }
